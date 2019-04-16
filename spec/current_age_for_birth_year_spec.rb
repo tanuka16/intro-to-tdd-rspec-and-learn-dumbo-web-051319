530f0530b979eb689796e8b2a55e02c796223ed0
Tanuka Das <das.tanuka@yahoo.com>
2019-04-16 06:45:40 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -3,11 +3,10 @@ def current_age_for_birth_year(birth_year)
   2003 - birth_year
 end
 describe "current_age_for_birth_year method" do             #describe method describes what we are testing
-#end                                                          #do is a Ruby keyword
+                                                         #do is a Ruby keyword
   it "returns the age of a person based on the year of birth" do          #it is an RSpec method, tate an expectation or behavior of that metho
     age_of_person = current_age_for_birth_year(1984)
 
     expect(age_of_person).to eq(19)
   end
 end
-#end
